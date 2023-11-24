9bbc8f21927b0b792c7b1b63c2ceb0b88effa1f5
SpamJacket <saealeksandrsae@mail.ru>
2023-11-24 19:53:48 +0300

    feat: add reversing string algorithm testing

---
+++ src/utils/tests/string.test.js
@@ -0,0 +1,37 @@
+import reverseString from "../string";
+
+describe("Алгоритм разворота", () => {
+  it("Алгоритм работает верно с чётным количеством символов", () => {
+    const sourceString = "123456";
+    const reverseRes = reverseString(sourceString.split(""));
+    expect(reverseRes).toEqual([
+      ["1", "2", "3", "4", "5", "6"],
+      ["6", "2", "3", "4", "5", "1"],
+      ["6", "5", "3", "4", "2", "1"],
+      ["6", "5", "4", "3", "2", "1"],
+    ]);
+  });
+
+  it("Алгоритм работает верно с нечетным количеством символов", () => {
+    const sourceString = "1234567";
+    const reverseRes = reverseString(sourceString.split(""));
+    expect(reverseRes).toEqual([
+      ["1", "2", "3", "4", "5", "6", "7"],
+      ["7", "2", "3", "4", "5", "6", "1"],
+      ["7", "6", "3", "4", "5", "2", "1"],
+      ["7", "6", "5", "4", "3", "2", "1"],
+    ]);
+  });
+
+  it("Алгоритм работает верно с одним символов", () => {
+    const sourceString = "1";
+    const reverseRes = reverseString(sourceString.split(""));
+    expect(reverseRes).toEqual([["1"]]);
+  });
+
+  it("Алгоритм работает верно с пустой строкой", () => {
+    const sourceString = "";
+    const reverseRes = reverseString(sourceString.split(""));
+    expect(reverseRes).toEqual([]);
+  });
+});
